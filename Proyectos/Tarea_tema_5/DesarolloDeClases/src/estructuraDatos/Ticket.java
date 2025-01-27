108cadf05d0425f9a53da4344c243be12ccc3d30
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2025-01-27 12:58:48 +0100

    empezando ticket

--- Proyectos/Tarea_tema_5/DesarolloDeClases/src/estructuraDatos/Ticket.java
+++ Proyectos/Tarea_tema_5/DesarolloDeClases/src/estructuraDatos/Ticket.java
@@ -1,6 +1,24 @@
 package estructuraDatos;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Ticket {
-    
+    private static short anioActual = (short) LocalDate.now().getYear();
+    private final LocalDate fecha;
+    private LocalTime usado;
+    private final int TIQUET_MAXIMO = 99999999;
+    private String ticket;
+    private static int acumulador;
+    public Ticket() {
+        for(int i = 0; i <= TIQUET_MAXIMO; i++){
+            ticket = ANIO_ACTUAL - i;
+            
+        }
+        return ticket;
+    }
+    public static Ticket(String){
+
+    }
     
 }
