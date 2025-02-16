f174017cdf3699141ef7865c80448554dc37d842
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2025-02-16 21:27:46 +0100

    libro digital

--- Proyectos/Tarea_tema_6/Jerarquia_Libreria/Libreria/src/EstructuraDatos/Libro.java
+++ Proyectos/Tarea_tema_6/Jerarquia_Libreria/Libreria/src/EstructuraDatos/Libro.java
@@ -6,8 +6,8 @@ public abstract class Libro extends Producto {
     private int anioPublicacion;
     private final int MIN_ANIO = 1500;
 
-    public libro(double precio, String DESCRIPCION, String tituloLibro, String autorLibro, int anioPublicacion){
-        super(precio, DESCRIPCION);
+    public libro(int IDENTIFICADOR, double precio, String DESCRIPCION, String tituloLibro, String autorLibro, int anioPublicacion) throws IllegalArgumentException {
+        super (IDENTIFICADOR, precio, descripcion);
         if(anioPublicacion >= MIN_ANIO){
 
         }
@@ -23,6 +23,20 @@ public abstract class Libro extends Producto {
 
     public int getAnioPublicacion() {
 
+    }
+
+    public Libro(String titulo, double precio, String descripcion, String autor, int year)
+            throws IllegalArgumentException {
+        super(precio, descripcion);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-14s titulo: %-14s autor:%-14s año:%-4d",
+                super.toString(),
+                this.getTituloLibro(),
+                this.getAutorLibro(),
+                this.getAnioPublicacion());
     }
 
 }
--- Proyectos/Tarea_tema_6/Jerarquia_Libreria/Libreria/src/EstructuraDatos/LibroFisico.java
+++ Proyectos/Tarea_tema_6/Jerarquia_Libreria/Libreria/src/EstructuraDatos/LibroFisico.java
@@ -1,5 +1,35 @@
 package EstructuraDatos;
 
-public class LibroFisico extends Libro {
-    
+public final class LibroFisico extends Libro {
+    private int numPaginas;
+    private final int MIN_PAGINAS = 20;
+    private final int MAX_PAGINAS = 5000;
+
+    public LibroFisico(int IDENTIFICADOR, double precio, String DESCRIPCION, String tituloLibro, String autorLibro,
+            int anioPublicacion, int numPaginas) {
+        super(IDENTIFICADOR, precio, DESCRIPCION, tituloLibro, autorLibro, anioPublicacion);
+
+        if (numPaginas < MIN_PAGINAS || numPaginas > MAX_PAGINAS) {
+            throw new IllegalArgumentException("Número de páginas fuera del rango permitido: " + numPaginas);
+        }
+
+        this.numPaginas = numPaginas;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public LibroFisico(int IDENTIFICADOR, String tituloLibro, double precio, String DESCRIPCION, String autorLibro,
+            int anioPublicacion)
+            throws IllegalArgumentException {
+        super.Libro();
+    }
+
+    @Override
+    final String toString() {
+        return String.format("%-14s NumPaginas %d",
+                super.toString(),
+                this.getNumPaginas());
+    }
 }
