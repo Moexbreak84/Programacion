2b0c08cb18b0690f0be1c863ce1720d772d468bc
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2025-04-03 10:36:32 +0200

    Pong falta foto

--- Proyectos/Pong/src/main/java/com/juego/App.java
+++ Proyectos/Pong/src/main/java/com/juego/App.java
@@ -1,6 +1,9 @@
 package com.juego;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,10 +12,19 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         // To do
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("ventanaJuego.fxml"));
+        AnchorPane root = loader.load();
+
+        Scene scene = new Scene(root, 600, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("PING PONG");
+        primaryStage.show();
+        root.requestFocus();
     }
 
     public static void main(String[] args) {
         // To do
+        launch();
     }
 
 }
--- Proyectos/Pong/src/main/java/com/juego/ControladorJuego.java
+++ Proyectos/Pong/src/main/java/com/juego/ControladorJuego.java
@@ -2,13 +2,30 @@ package com.juego;
 
 import javafx.animation.AnimationTimer;
 import javafx.animation.PauseTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class ControladorJuego {
     // To do
+    @FXML
+    private Circle bola;
+    @FXML
+    private AnchorPane contenedor;
+    @FXML
+    private Rectangle jugadorA;
+    @FXML
+    private Rectangle jugadorB;
+    @FXML
+    private Label marcadorLabel;
     private AnimationTimer gameLoop;
+    private int bolaVelocidadY = 3;
+    private int bolaVelocidadX = 3;
 
     public void initialize() {
         PauseTransition pause = new PauseTransition(Duration.seconds(2));
@@ -29,10 +46,22 @@ public class ControladorJuego {
 
     private void movimientoBola() {
         // To do
+        bola.setCenterX(bola.getCenterX() + bolaVelocidadX);
+        bola.setCenterY(bola.getCenterY() + bolaVelocidadY);
     }
 
     private void comprobarColisiones() {
         // To do
+        if ()
+
+        // Puntuacion
+        if(bola.getCenterX() <=0){
+            puntoJ2*++;
+            resetBola();
+        }else if(bola.getCenterY() >= 600){
+            puntoJ1*++;
+            resetBola();
+        }
     }
 
     private void reiniciarBola() {
@@ -44,7 +73,7 @@ public class ControladorJuego {
         pause.play();
     }
 
-
+    @FXML
     private void mover(KeyEvent event) {
         // To do
     }
