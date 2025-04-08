29dc3ba39c11117d8a502bb6ed7788eb8d3eb7fe
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2025-04-08 20:05:13 +0200

    pong falta simultaneo

--- Proyectos/Pong/src/main/java/com/juego/ControladorJuego.java
+++ Proyectos/Pong/src/main/java/com/juego/ControladorJuego.java
@@ -1,6 +1,9 @@
 
 package com.juego;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javafx.animation.AnimationTimer;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
@@ -24,6 +27,7 @@ public class ControladorJuego {
     private Rectangle jugadorB;
     @FXML
     private Label marcadorLabel;
+
     private AnimationTimer gameLoop;
     private int bolaVelocidadY = 3;
     private int bolaVelocidadX = 3;
@@ -31,8 +35,8 @@ public class ControladorJuego {
     int puntoJ2 = 0;
 
     public void initialize() {
-        //bola.setLayoutX(100);
-        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+        // bola.setLayoutX(100);
+        PauseTransition pause = new PauseTransition(Duration.seconds(6));
         pause.setOnFinished(event -> startGame());
         pause.play();
     }
@@ -43,6 +47,7 @@ public class ControladorJuego {
             public void handle(long now) {
                 movimientoBola();
                 comprobarColisiones();
+
             }
         };
         gameLoop.start();
@@ -55,57 +60,63 @@ public class ControladorJuego {
 
     private void comprobarColisiones() {
         // Revote contra las paredes arriba y abajo
-        if (bola.getCenterY() <=-300 || bola.getCenterY() >=300){
+        if (bola.getCenterY() <= -300 || bola.getCenterY() >= 300) {
             bolaVelocidadY *= -1;
         }
 
-        if(bola.getBoundsInParent().intersects(jugadorA.getBoundsInParent()) || bola.getBoundsInParent().intersects(jugadorB.getBoundsInParent())){
+        if (bola.getBoundsInParent().intersects(jugadorA.getBoundsInParent())
+                || bola.getBoundsInParent().intersects(jugadorB.getBoundsInParent())) {
             bolaVelocidadX *= -1;
         }
 
         // Puntuacion
-        if(bola.getCenterX() <=0){
-            
+        if (bola.getCenterX() <= 0) {
+
             puntoJ2++;
             reiniciarBola();
-        }else if(bola.getCenterY() >= 600){
-            
+        } else if (bola.getCenterY() >= 600) {
+
             puntoJ1++;
-            reiniciarBola();  
+            reiniciarBola();
         }
     }
 
     private void reiniciarBola() {
         // Pausa antes de reiniciar el movimiento
         gameLoop.stop();
-        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+        PauseTransition pause = new PauseTransition(Duration.seconds(6));
         pause.setOnFinished(event -> gameLoop.start());
         pause.play();
     }
 
     @FXML
     private void mover(KeyEvent event) {
-            teclaPresionada(event.getCode()); 
+        teclaPresionada(event.getCode());
     }
 
     private void teclaPresionada(KeyCode tecla) {
-        // Definir los límites de movimiento
-    double limiteSuperior = 0; // Por ejemplo, el borde superior de la ventana
-    double limiteInferior = 600; // Por ejemplo, el borde inferior de la ventana (ajusta según tu diseño)
-
-        if(tecla == KeyCode.W){
-            jugadorA.setLayoutY(jugadorA.getLayoutY()-10 );
-        }else if(tecla == KeyCode.S){
-            jugadorA.setLayoutY(jugadorA.getLayoutY()+10 );
-            
+        // declaracion limites para jugadores
+        int limiteSuperior = 1;
+        int limiteInferior = 501;
+        // movimiento con limites pero falta que funcionen simultaneos
+        if (tecla == KeyCode.W) {
+            if (jugadorA.getLayoutY() > limiteSuperior) {
+                jugadorA.setLayoutY(jugadorA.getLayoutY() - 30);
+            }
+        } else if (tecla == KeyCode.S) {
+            if (jugadorA.getLayoutY() < limiteInferior) {
+                jugadorA.setLayoutY(jugadorA.getLayoutY() + 30);
+            }
         }
-        if(tecla == KeyCode.UP){
-            jugadorB.setLayoutY(jugadorB.getLayoutY()-10 );
-        }else if(tecla == KeyCode.DOWN){
-            jugadorB.setLayoutY(jugadorB.getLayoutY()+10 );
-            
+        if (tecla == KeyCode.UP) {
+            if (jugadorB.getLayoutY() > limiteSuperior) {
+                jugadorB.setLayoutY(jugadorB.getLayoutY() - 30);
+            }
+        } else if (tecla == KeyCode.DOWN) {
+            if (jugadorB.getLayoutY() < limiteInferior) {
+                jugadorB.setLayoutY(jugadorB.getLayoutY() + 30);
+            }
         }
-
     }
 
 }
