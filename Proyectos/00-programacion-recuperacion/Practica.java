a8e21d5eabdc4b827a26444ccda6a7c4ace7c5b5
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2025-06-04 22:21:12 +0200

    array

--- Proyectos/00-programacion-recuperacion/Practica.java
+++ Proyectos/00-programacion-recuperacion/Practica.java
@@ -1,7 +1,23 @@
 public class Practica {
 
     public static void main(String[] args) {
-        
+        int[] numeros = new int[10];
+
+        numeros[0] = 5;
+        numeros[1] = 6;
+        numeros[2] = 7;
+        numeros[3] = 8;
+        numeros[4] = 9;
+        numeros[5] = 10;
+        numeros[5] = 10;
+
+        System.out.println("tamaño del array" + numeros.length);
+
+        for (int i = 00; i < numeros.length; i++) {
+            System.out.println("valor: " + numeros[i]);
+
+
+        }
     }
 
 }
