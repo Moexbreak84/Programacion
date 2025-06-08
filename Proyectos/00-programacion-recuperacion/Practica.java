e0b8a20e745437c7b0fa9576b32a942820f31619
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2025-06-08 20:01:35 +0200

    practicas

--- Proyectos/00-programacion-recuperacion/Practica.java
+++ Proyectos/00-programacion-recuperacion/Practica.java
@@ -1,7 +1,21 @@
 public class Practica {
 
     public static void main(String[] args) {
-        
+        i
+    
+        int[] numeros = new int[10];
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+        numeros[5] = 6;
+        numeros[6] = 7;
+        numeros[7] = 8;
+
+        for(int i = 0; i <= numeros.length; i++) {
+            System.out.println("el numero del array " +numeros[i]);
+        }
     }
 
 }
---
+++ Proyectos/000Repaso/Churrero/src/App.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    static void leerEnteros(int numero){
+        ArrayList<Integer> numeros = new ArrayList<>(numero);
+        for(int i = 0; i < numero; i++) {
+            System.out.println("Introduce el numero " + (i + 1) + ": ");
+            numeros.add(sc.nextInt());
+        }   
+        System.out.println("Los numeros introducidos son: ");
+        for(int i = 0; i < numeros.size(); i++) {
+            System.out.println("El numero del array " + numeros.get(i));
+        }
+    }
+}
